577e4c788fd38597fedcd5b34b032d456241caa0
Toma John Musa <tomajohn316@yahoo.com>
2023-07-28 12:00:57 +0100

    Fix Faq

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,5 +1,6 @@
 import { Tabs, TabsHeader, Tab, TabsBody, TabPanel } from "@material-tailwind/react";
 import { useState } from "react";
+import FaqComp from "./FAQComp";
 
 const Faq = () => {
   const [activeTab, setActiveTab] = useState("farm");
@@ -7,17 +8,110 @@ const Faq = () => {
     {
       label: "Farmer Questions",
       value: "farm",
-      desc: "farmers"
+      desc: [
+        {
+          id: 1,
+          question: "What is OneFarm?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 2,
+          question: "How does OneFarm benefit retailers?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 3,
+          question: "How does OneFarm benefit farmers?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 4,
+          question: "How does OneFarm ensure fair revenue distribution?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 5,
+          question: "What crops and products are available on OneFarm?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 6,
+          question: "How can I join OneFarm as a farmer or retailer?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        }
+      ]
     },
     {
       label: "Middleman Questions",
       value: "middle",
-      desc: "Middle"
+      desc: [
+        {
+          id: 1,
+          question: "What is OneFarm?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 2,
+          question: "How does OneFarm benefit retailers?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 3,
+          question: "How does OneFarm benefit farmers?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 4,
+          question: "How does OneFarm ensure fair revenue distribution?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 5,
+          question: "What crops and products are available on OneFarm?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 6,
+          question: "How can I join OneFarm as a farmer or retailer?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        }
+      ]
     },
     {
       label: "Retailer Questions",
       value: "retail",
-      desc: "retailers"
+      desc: [
+        {
+          id: 1,
+          question: "What is OneFarm?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 2,
+          question: "How does OneFarm benefit retailers?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 3,
+          question: "How does OneFarm benefit farmers?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 4,
+          question: "How does OneFarm ensure fair revenue distribution?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 5,
+          question: "What crops and products are available on OneFarm?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        },
+        {
+          id: 6,
+          question: "How can I join OneFarm as a farmer or retailer?",
+          answer: "OneFarm is a technology-driven platform that empowers smallholder farmers in Nigeria by connecting them directly with retailers, streamlining the agricultural supply chain, and ensuring fair revenue distribution.",
+        }
+      ]
     }
   ];
 
@@ -45,7 +139,13 @@ const Faq = () => {
         <TabsBody>
           {data.map(({ value, desc }) => (
             <TabPanel key={value} value={value}>
-              {desc}
+              <div>
+                {
+                  desc.map((faq) => (
+                    <FaqComp key={faq.id} answer={faq.answer} question={faq.question} />
+                  ))
+                }
+              </div>
             </TabPanel>
           ))}
         </TabsBody>
