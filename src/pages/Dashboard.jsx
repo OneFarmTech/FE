de08fc98060560fc4e3b53cb79f804d3efc0afaa
Toma John Musa <tomajohn316@yahoo.com>
2023-09-23 11:33:30 +0100

    Fix title issues

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,8 +6,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "../redux/user/userSlice";
 
 const Dashboard = () => {
-  const user = useSelector((state) => (state.user.user));
-  const [ name, setHeader ] = useState(user.name);
+  const {user} = useSelector((state) => (state.user));
+  const [ name, setHeader ] = useState(`Hello ${user.name}`);
   const dispatch = useDispatch();
 
   const changeHeading = (title) => {
@@ -19,15 +19,15 @@ const Dashboard = () => {
     console.log(user);
   }, [dispatch, fetchUser]);
   
-  const resetHeading = (title) => {
-    setHeader('Hello Prince');
+  const resetHeading = () => {
+    setHeader(`Hello ${user.name}`);
   }
 
   return (
     <>
     <DashNav />
       <div className="2xl:pl-[16%] pt-[310px] lg:pt-[136px] relative bg-[#f9f9f9] h-screen w-full">
-        <DashHeader title={`Hello ${name}`} />
+        <DashHeader title={name} />
         <div className="overflow-y-auto bg-[#f9f9f9]">
         <Outlet context={[changeHeading, resetHeading]} />
         </div>
