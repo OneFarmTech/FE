aa717ff510c6f9ee83aa7e9423d14280862d5408
Toma John Musa <tomajohn316@yahoo.com>
2023-10-26 16:11:54 +0100

    fix bugs on products page

--- src/pages/ProductRoot.jsx
+++ src/pages/ProductRoot.jsx
@@ -1,9 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import ProductsHeader from "../components/WaitingList/ProductsHeader";
 import Footer from "../components/Footer";
 
 const ProductRoot = () => (
   <>
+    <ScrollRestoration />
     <ProductsHeader />
 
     <Outlet />
