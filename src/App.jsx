7b344169871792cbad96f259309bf953b04ec095
Toma John Musa <tomajohn316@yahoo.com>
2023-10-30 20:40:18 +0100

    Fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import Root from "./pages/Root";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-// import FarmerSolution from "./pages/FarmerSolution";
-// import RetailSolution from "./pages/RetailSolution";
-// import MiddleSolution from "./pages/MiddleSolution";
-// import AuthRoot from "./pages/AuthRoot";
-// import Dashboard from "./pages/Dashboard";
-// import DashboardHome from "./pages/DashboardHome";
-// import Kyc from "./pages/Kyc";
-// import Profile from "./pages/Profile";
-// import { useSelector } from "react-redux";
+import FarmerSolution from "./pages/FarmerSolution";
+import RetailSolution from "./pages/RetailSolution";
+import MiddleSolution from "./pages/MiddleSolution";
+import AuthRoot from "./pages/AuthRoot";
+import Dashboard from "./pages/Dashboard";
+import DashboardHome from "./pages/DashboardHome";
+import Kyc from "./pages/Kyc";
+import Profile from "./pages/Profile";
+import { useSelector } from "react-redux";
 import WaitingList from "./pages/WaitingList";
 import Products from "./pages/Products";
 import ProductRoot from "./pages/ProductRoot";
@@ -21,7 +21,7 @@ import MarketPlace from "./pages/MarketPlace";
 import NewProduct from "./pages/NewProduct";
 
 const App = () => {
-  // const {userToken} = useSelector((state) => (state.register));
+  const {userToken} = useSelector((state) => (state.register));
 
   const router1 = createBrowserRouter([
     {
