def0a2f340c1412ee73d9cc2f2db93c98ce4455f
salissadiq <salissadiq@gmail.com>
2024-01-22 23:42:30 +0100

    Add react query in sign process, redirect, and visibility control

--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -6,13 +6,17 @@ import RetailProduct from "../components/marketplace/RetailProd";
 import { useOutletContext } from "react-router-dom";
 import QueryClient from "../js/QueryClient";
 
+import { useDispatch, useSelector } from "react-redux";
+
+
 const MarketPlace = () => {
   const [changeHeading, resetHeading] = useOutletContext();
   const [myProducts, setMyProducts] = useState([]);
   const [Products, setProducts] = useState([]);
-  
-  let role = sessionStorage.getItem('role');
 
+  // let role = sessionStorage.getItem('role');
+  const user = useSelector((state) => (state.user));
+  const { userDetails, error, role } = user;
   useEffect(() => {
     changeHeading("MarketPlace");
     return () => {
@@ -33,7 +37,7 @@ const MarketPlace = () => {
         console.log(error);
       });
 
-      client
+    client
       .get("myproducts")
       .then((data) => {
         console.log(data[0].id);
@@ -43,15 +47,15 @@ const MarketPlace = () => {
         console.log(error);
       });
   }, []);
-
+  console.log('>>>> ', role);
   return (
     <section className="px-[4%] py-4 flex flex-col w-full h-full">
-       {role === 'retailer' ? (
-       <RetailTop />
+      {role === 'retailer' ? (
+        <RetailTop />
       ) : (
-        <TopSection />
+        <TopSection role={role} />
       )}
-     
+
 
       <div className="mt-10 flex flex-col gap-8 lg:flex-row lg:gap-10">
         <div className="flex flex-col gap-8 w-full max-w-[42rem]">
@@ -69,36 +73,36 @@ const MarketPlace = () => {
           </div>
         </div>
         {role === 'retailer' ? (
-     ''
-      ) : (
-        <div className="flex flex-col gap-5 w-full max-w-[27rem] lg:pt-10">
-       <div className="flex justify-between items-center">
-         <h2 className="text-2xl font-bold">Market Insights</h2>
-         <button className="underline text-base">View more</button>
-       </div>
+          ''
+        ) : (
+          <div className="flex flex-col gap-5 w-full max-w-[27rem] lg:pt-10">
+            <div className="flex justify-between items-center">
+              <h2 className="text-2xl font-bold">Market Insights</h2>
+              <button className="underline text-base">View more</button>
+            </div>
 
-       <div className="p-5 bg-white shadow-md rounded-md">
-         <ul className="flex flex-col gap-3">
-           <li>
-             Demand for rice{" "}
-             <span className="text-green-30">(High 30%)</span>
-           </li>
-           <li>
-             Demand for sugarcane{" "}
-             <span className="text-green-30">(High 92%)</span>
-           </li>
-           <li>
-             Demand for palm oil{" "}
-             <span className="text-orange-30">(High 8%)</span>
-           </li>
-           <li>
-             Demand for corn <span className="text-red-50">(Low -0.9%)</span>
-           </li>
-         </ul>
-       </div>
-     </div>
-      )}
-        
+            <div className="p-5 bg-white shadow-md rounded-md">
+              <ul className="flex flex-col gap-3">
+                <li>
+                  Demand for rice{" "}
+                  <span className="text-green-30">(High 30%)</span>
+                </li>
+                <li>
+                  Demand for sugarcane{" "}
+                  <span className="text-green-30">(High 92%)</span>
+                </li>
+                <li>
+                  Demand for palm oil{" "}
+                  <span className="text-orange-30">(High 8%)</span>
+                </li>
+                <li>
+                  Demand for corn <span className="text-red-50">(Low -0.9%)</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+
       </div>
     </section>
   );
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SocialIcons from "../components/SocialIcons";
 import { useState } from "react";
 import { clearUser, codeVerification, signupThunk } from "../redux/register/registerSlice";
@@ -7,9 +7,12 @@ import ChangePage from "../components/pageChange/SwitchPage";
 import InputValidation from "../components/pageChange/InputValidation";
 import ErrorMessage from "../components/pageChange/ErrorMessage";
 import Counter from "../components/pageChange/Counter";
-
+import { usePOST } from "../hooks/usePOST.hook";
 const Signup = () => {
+  const navigate = useNavigate();
   const register = useSelector((state) => (state.register));
+  const [showVerifyOTP, setShowVerifyOTP] = useState(false)
+
   const dispatch = useDispatch();
   const [otp, setOtp] = useState('');
   const [signupDetails, setDetails] = useState({
@@ -23,6 +26,7 @@ const Signup = () => {
     message: ''
   });
 
+  const { mutate, isPending, isError, isSuccess } = usePOST('register', false)
   const handleChange = (e) => {
     let newKey = e.currentTarget.name;
     let val = e.currentTarget.value;
@@ -32,7 +36,7 @@ const Signup = () => {
       return;
     }
 
-    setDetails((state) => ({...state, [newKey]: val}));
+    setDetails((state) => ({ ...state, [newKey]: val }));
   }
 
   const handleSignUp = (e) => {
@@ -55,8 +59,19 @@ const Signup = () => {
         return;
       }
     }
-    
-    dispatch(signupThunk(signupDetails));
+
+    // dispatch(signupThunk(signupDetails));
+    mutate(signupDetails, {
+      onSuccess: (returnData) => {
+        if (returnData.message === 'Account Already Exist') {
+          return navigate('/auth/login')
+        }
+        setShowVerifyOTP(true)
+      },
+      onError: (error) => {
+        console.log(error);
+      }
+    })
   };
 
   const submitOtp = (e) => {
@@ -79,39 +94,52 @@ const Signup = () => {
       return;
     }
 
-    dispatch(codeVerification({
+    // dispatch(codeVerification({
+    //   otp: otp,
+    //   phone: signupDetails.phone
+    // }));
+    mutate({
       otp: otp,
       phone: signupDetails.phone
-    }));
+    }, {
+      onSuccess: (returnData) => {
+        console.log('>>>', returnData);
+        sessionStorage.setItem("token", returnData.token)
+        navigate('/dashboard/home')
+      },
+      onError: (error) => {
+        console.log(error);
+      }
+    })
   }
 
   return (
     <section className="flex flex-col w-[90%] max-w-5xl gap-10 mx-auto">
-    <h1 className="text-5xl text-center lg:text-left leading-[3.2rem]">Sign Up To Create An Account</h1>
+      <h1 className="text-5xl text-center lg:text-left leading-[3.2rem]">Sign Up To Create An Account</h1>
 
-    <ChangePage page1={register.response == null} firstTitle='Personal Details' secTitle='Verification' />
+      <ChangePage page1={register.response == null} firstTitle='Personal Details' secTitle='Verification' />
 
-    { invalid.error && (<InputValidation message={invalid.message} />) }
+      {invalid.error && (<InputValidation message={invalid.message} />)}
 
-    { register.error && (<ErrorMessage />) }
-    
-     { register.response == null && (<form className="flex flex-col gap-5">
+      {register.error && (<ErrorMessage />)}
+
+      {!showVerifyOTP && (<form className="flex flex-col gap-5">
 
         <div className="flex flex-col gap-6 lg:flex-row lg:gap-2 lg:justify-between items-center">
           <div className="flex flex-col gap-6 w-full lg:w-[50%]">
             <div className="flex flex-col gap-4">
               <label htmlFor="name" className="font-bold">Name</label>
-              <input onChange={handleChange} className="pl-3 bg-transparent border border-[#C7CDD2] p-3" type="text" id="name" name="name" placeholder="Enter Your Full Name" value={signupDetails.name} />
+              <input onChange={handleChange} className="pl-3 bg-transparent border border-[#C7CDD2] p-3" type="text" id="name" name="name" placeholder="Enter Your Full Name" value={signupDetails.name} required />
             </div>
 
             <div className="flex flex-col gap-4">
               <label htmlFor="phone" className="font-bold">Phone Number</label>
-              <input onChange={handleChange} className="pl-3 bg-transparent border border-[#C7CDD2] p-3 lg:flex-[70%]" type="text" id="phone" name="phone" placeholder="Enter Your Phone Number" value={signupDetails.phone} />
+              <input onChange={handleChange} className="pl-3 bg-transparent border border-[#C7CDD2] p-3 lg:flex-[70%]" type="text" id="phone" name="phone" placeholder="Enter Your Phone Number" value={signupDetails.phone} required />
             </div>
 
             <div className="flex flex-col gap-4">
               <label htmlFor="email" className="font-bold">Email</label>
-              <input onChange={handleChange} className="pl-3 bg-transparent border border-[#C7CDD2] p-3 lg:flex-[70%]" type="email" id="email" name="email" placeholder="Enter Your Email" value={signupDetails.email} />
+              <input onChange={handleChange} className="pl-3 bg-transparent border border-[#C7CDD2] p-3 lg:flex-[70%]" type="email" id="email" name="email" placeholder="Enter Your Email" value={signupDetails.email} required />
             </div>
 
           </div>
@@ -125,38 +153,38 @@ const Signup = () => {
 
         <div className="flex justify-center w-full lg:justify-between pr-3 lg:max-w-lg">
           <Link to='/' className="flex items-center text-white py-2 px-5 lg:px-9 bg-black-100 lg:block">Go Back</Link>
-          <button className="text-white px-9 bg-green-30 py-3" onClick={handleSignUp} type="submit">Get Verification Code</button>
+          <button className="text-white px-9 bg-green-30 py-3" onClick={handleSignUp} disabled={isPending} type="submit">{isPending ? 'Please wait.....' : 'Get Verification Code'}</button>
         </div>
       </form>)
-}
-  { register.response != null && (<form className="flex flex-col gap-5" onSubmit={submitOtp}>
-      <div className="flex flex-col gap-6 lg:flex-row lg:gap-2 lg:justify-between items-center">
-        <div className="flex flex-col gap-6 lg:w-[50%]">
-          <div className="flex flex-col gap-4">
-            <label htmlFor="code" className="font-bold">Verification Code</label>
-            <div className="flex">
-              <input onChange={handleChange} className="w-full pl-3 bg-transparent border border-[#C7CDD2] p-3 lg:flex-[70%]" type="number" id="otp" name="otp" placeholder="Enter code sent to email or phone number" />
+      }
+      {showVerifyOTP && (<form className="flex flex-col gap-5" onSubmit={submitOtp}>
+        <div className="flex flex-col gap-6 lg:flex-row lg:gap-2 lg:justify-between items-center">
+          <div className="flex flex-col gap-6 lg:w-[50%]">
+            <div className="flex flex-col gap-4">
+              <label htmlFor="code" className="font-bold">Verification Code</label>
+              <div className="flex">
+                <input onChange={handleChange} className="w-full pl-3 bg-transparent border border-[#C7CDD2] p-3 lg:flex-[70%]" type="number" id="otp" name="otp" placeholder="Enter code sent to email or phone number" />
+              </div>
+            </div>
+
+            <div className="italic flex flex-col gap-3">
+              <p>We just sent a code to you. It may take a minute to receive your code</p>
+              <p>Haven’t recieved it? <Counter login={handleSignUp} /></p>
             </div>
           </div>
 
-          <div className="italic flex flex-col gap-3">
-            <p>We just sent a code to you. It may take a minute to receive your code</p>
-            <p>Haven’t recieved it? <Counter login={handleSignUp} /></p>
+          <div className="font-bold max-w-md flex lg:w-[30%] flex-col gap-5">
+
           </div>
         </div>
 
-        <div className="font-bold max-w-md flex lg:w-[30%] flex-col gap-5">
-          
+        <div className="flex w-full justify-between lg:pr-3 lg:max-w-lg">
+          <button className="text-white py-2 px-5 lg:px-9 bg-black-100 lg:block" onClick={() => {
+            dispatch(clearUser());
+          }}>Go Back</button>
+          <button className="text-white px-5 lg:px-9 bg-green-30 py-3" type="submit" disabled={isPending} >{isPending ? 'Please wait.....' : 'Sign up'}</button>
         </div>
-      </div>
-
-      <div className="flex w-full justify-between lg:pr-3 lg:max-w-lg">
-        <button className="text-white py-2 px-5 lg:px-9 bg-black-100 lg:block" onClick={() => {
-          dispatch(clearUser());
-        }}>Go Back</button>
-        <button className="text-white px-5 lg:px-9 bg-green-30 py-3" type="submit">Sign Up</button>
-      </div>
-    </form>)}
+      </form>)}
 
       <div className="font-bold text-lg text-center">
         Already have an account? <Link className="text-green-30 pl-2" to='/auth/login' onClick={() => {
